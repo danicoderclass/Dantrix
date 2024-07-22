5c7c4cfc19ea197e8bfe2ccd178a413f9de4d178
danicoderclass <129858277+danicoderclass@users.noreply.github.com>
2024-07-22 12:56:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message, Dantrix will get back to you soon!');
+    // Add form submission functionality here (e.g., sending data to an email or server)
+});
+
